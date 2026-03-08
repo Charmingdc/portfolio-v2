681685e379ccf676c86236ead4086996d73f9fa6
Charmingdc <charmingdc002@gmail.com>
2026-03-08 11:37:06 +0100

    feat(player): add music player with embedded songs

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,25 @@ import { lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import ScrollToHash from "@/components/shared/ScrollToHash";
 import Layout from "@/components/shared/Layout";
+import MusicPlayer from "@/components/shared/MusicPlayer";
 import MainPage from "@/pages/MainPage";
 const ProjectsPage = lazy(() => import("@/pages/ProjectsPage"));
 
 const App = () => {
-  return (
-    <>
-      <ScrollToHash />
+ return (
+  <>
+   <ScrollToHash />
+   <MusicPlayer />
 
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/projects" element={<ProjectsPage />} />
-          <Route path="/projects/:name" element={<ProjectsPage />} />
-        </Route>
-      </Routes>
-    </>
-  );
+   <Routes>
+    <Route element={<Layout />}>
+     <Route path="/" element={<MainPage />} />
+     <Route path="/projects" element={<ProjectsPage />} />
+     <Route path="/projects/:name" element={<ProjectsPage />} />
+    </Route>
+   </Routes>
+  </>
+ );
 };
 
 export default App;
--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -5,12 +5,10 @@ import ProjectsList from "@/components/projects-page/ProjectsList";
 import ProjectOverview from "@/components/projects-page/ProjectOverview";
 
 const ProjectsPage = () => {
-  useScrollToTop();
-  const { name } = useParams();
+ useScrollToTop();
+ const { name } = useParams();
 
-  return (
-    <>{name ? <ProjectOverview projectName={name} /> : <ProjectsList />}</>
-  );
+ return <>{name ? <ProjectOverview projectName={name} /> : <ProjectsList />}</>;
 };
 
 export default ProjectsPage;
